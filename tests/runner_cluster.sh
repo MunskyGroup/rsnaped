5f63720d227e42da206f60854d2905e8d6214cdf
luisub <>
2021-04-05 16:28:59 -0600

    chanes to cluster commands

--- tests/runner_cluster.sh
+++ tests/runner_cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #$ -cwd
 #$ -N rsnaped_job
-#$ -e error_rsnaped.er 
+#$ -e error_rsnaped.er
+#$ -o test_o.txt 
 #$ -q munsky-gpu.q@gpu*
 
 # module purge
@@ -9,5 +10,5 @@
 # module load conda/10.0
 
 #source /home/students/luisub/.conda/envs/t0/bin/activate
-/home/students/luisub/.conda/envs/rsnaped_env/bin/python3 ./simulation_tracking.py >> out.txt
+/home/students/bsilagy/.conda/envs/rsnaped_env/bin/python3 ./simulation_tracking.py 20 40 >> out.txt
 #python simulation_tracking.py 20 40 >> out.txt
