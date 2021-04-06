bf7062dde7e1e311f5c064f5e078cf8340793f79
luisub <>
2021-04-06 13:45:50 -0600

    changes to cluster commands

--- tests/runner_cluster.sh
+++ tests/runner_cluster.sh
@@ -10,5 +10,5 @@
 # module load conda/10.0
 
 #source /home/students/luisub/.conda/envs/t0/bin/activate
-/home/students/bsilagy/.conda/envs/rsnaped_env/bin/python3 ./simulation_tracking.py 20 40 >> out.txt
+/home/students/luisub/.conda/envs/rsnaped_env/bin/python3 ./simulation_tracking.py 20 40 >> out.txt
 #python simulation_tracking.py 20 40 >> out.txt
